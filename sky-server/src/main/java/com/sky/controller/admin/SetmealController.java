b4f328f5136758195fa19440c38cf31864d64316
kang.cao <1457857389@qq.com>
2024-02-01 12:14:05 +0800

    spring cache实现套餐缓存

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -12,6 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,6 +40,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId")     // 精确清理套餐的缓存数据
     public Result addSetmeal(@RequestBody SetmealDTO setmealDTO) {
         log.info("新增套餐: {}", setmealDTO);
         setmealService.addSetmeal(setmealDTO);
@@ -68,6 +70,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @ApiOperation("套餐的批量删除")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)     // 删掉setmealCache下所有的缓存数据
     public Result deleteBatch(@RequestParam List<Long> ids) {    // 请求参数是集合的形式，加上注解@RequestParam
         log.info("套餐的批量删除: {}", ids);
         setmealService.deleteBatch(ids);
@@ -96,6 +99,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)     // 删掉setmealCache下所有的缓存数据
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         log.info("修改套餐: {}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -111,6 +115,7 @@ public class SetmealController {
      */
     @PostMapping("/status/{status}")
     @ApiOperation("套餐的启售、停售")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)     // 删掉setmealCache下所有的缓存数据
     public Result startOrStop(@PathVariable Integer status, Long id) {  // TODO 忘记注解@PathVariable的作用了，应该是对应路径参数，再去看看
         log.info("套餐的启售、停售: {}, id: {}", status, id); // status中0停售 1启售     id是套餐id
         setmealService.startOrStop(status, id);
